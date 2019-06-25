f7cc08736ae5f6225d1ffe4e6ad6d2a8ee39a059
Rainblower <red5757687@bk.ru>
2019-06-25 12:45:14 +0300

    Add somthing

---
+++ Deno/Car.swift
@@ -0,0 +1,9 @@
+//
+//  Car.swift
+//  Deno
+//
+//  Created by WSR on 25/06/2019.
+//  Copyright © 2019 WSR. All rights reserved.
+//
+
+import Foundation
---
+++ Deno/Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Deno
+//
+//  Created by WSR on 25/06/2019.
+//  Copyright © 2019 WSR. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
