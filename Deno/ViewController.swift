08abe9fd7aad7fedd4269920945b785b75018d9d
Rainblower <red5757687@bk.ru>
2019-06-25 09:52:14 +0300

    init

---
+++ Deno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deno
+//
+//  Created by WSR on 25/06/2019.
+//  Copyright © 2019 WSR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
